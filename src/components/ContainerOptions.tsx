a5623723a52a5a78eca37a03fa9fa1a3293aead6
ViniHGV <ixxvinicius@gmail.com>
2023-11-15 20:42:30 -0300

    Arrumando Container Options

--- src/components/ContainerOptions.tsx
+++ src/components/ContainerOptions.tsx
@@ -1,3 +1,4 @@
+"use client";
 import React from "react";
 import { IContainerOptions } from "../../types/types";
 
@@ -7,9 +8,19 @@ export const ContainerOptions = ({
   isCheckbox,
 }: IContainerOptions) => {
   return (
-    <div className="flex flex-col gap-3 w-full border-b border-gray-300 pb-3">
-      <p className="w-full font-medium">{title}</p>
-      <div className="flex gap-2 w-full items-center px-4 justify-between">
+    <div
+      className={`flex flex-col gap-2 w-full ${
+        isCheckbox && "px-4 border-b border-gray-300 pb-3"
+      }`}
+    >
+      <p className={`w-full font-semibold text-sm ${!isCheckbox && "pl-4"}`}>
+        {title}
+      </p>
+      <div
+        className={`flex gap-2 w-full items-center ${
+          isCheckbox && " justify-between"
+        }`}
+      >
         {children}
       </div>
     </div>

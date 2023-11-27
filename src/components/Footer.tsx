9b8cf1d27fc82fc6ef76e2093f5ad9a2f2402fac
ViniHGV <ixxvinicius@gmail.com>
2023-11-27 19:08:08 -0300

    style: refatorando styles para mobile

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export const Footer = () => {
   return (
-    <div className="flex border-t border-[#C8C8C8] justify-between mt-16 px-36 py-8">
+    <div className="flex border-t border-[#C8C8C8] justify-between mt-16 px-5 flex-col text-center gap-4 lg:gap-0 lg:flex-row lg:px-36 py-8">
       <p className="font-medium">
         © 2023 ViniHGV. Todos os direitos reservados
       </p>

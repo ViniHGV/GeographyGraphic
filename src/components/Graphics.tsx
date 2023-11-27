4f1ec06907e2718d17a9cc46b83b8c83d1243e2a
ViniHGV <ixxvinicius@gmail.com>
2023-11-27 19:09:27 -0300

    feat: adicionando gerenciamento de estado de acordo com window

--- src/components/Graphics.tsx
+++ src/components/Graphics.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Chart from "react-apexcharts";
 import { IGraphics, TestType } from "../../types/types";
 
@@ -8,16 +8,33 @@ export const Graphics = ({
   dataNumbers,
   dataDescriptions,
 }: IGraphics) => {
+  const [graphicHorizontal, setGraphicHorizontal] = useState(false);
+
   let test: TestType = {
     series: dataNumbers,
     labels: dataDescriptions,
   };
+
+  useEffect(() => {
+    if (window) {
+      window.innerWidth <= 1024
+        ? setGraphicHorizontal(true)
+        : setGraphicHorizontal(false);
+    }
+  }, []);
 
   typeChart != "pie"
     ? (test = {
         options: {
           chart: {
             id: "basic-bar",
+          },
+          //Controle de direção de grafico Horizontal etc...
+          plotOptions: {
+            bar: {
+              borderRadius: 4,
+              horizontal: graphicHorizontal,
+            },
           },
           colors: ["#000"],
           xaxis: { categories: dataDescriptions },

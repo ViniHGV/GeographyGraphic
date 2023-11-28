9c51f4c6f901ae1843b8058492c0f05a62b88016
ViniHGV <ixxvinicius@gmail.com>
2023-11-28 18:24:20 -0300

    feat: adicionando funcionalidade por mudança de tamanho de tela

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,58 +1,13 @@
+"use client";
+import { useEffect, useState } from "react";
 import {
   Table,
   TableBody,
-  TableCaption,
   TableCell,
-  TableFooter,
   TableHead,
   TableHeader,
   TableRow,
 } from "@/components/ui/table";
-
-const invoices = [
-  {
-    invoice: "INV001",
-    paymentStatus: "Paid",
-    totalAmount: "$250.00",
-    paymentMethod: "Credit Card",
-  },
-  {
-    invoice: "INV002",
-    paymentStatus: "Pending",
-    totalAmount: "$150.00",
-    paymentMethod: "PayPal",
-  },
-  {
-    invoice: "INV003",
-    paymentStatus: "Unpaid",
-    totalAmount: "$350.00",
-    paymentMethod: "Bank Transfer",
-  },
-  {
-    invoice: "INV004",
-    paymentStatus: "Paid",
-    totalAmount: "$450.00",
-    paymentMethod: "Credit Card",
-  },
-  {
-    invoice: "INV005",
-    paymentStatus: "Paid",
-    totalAmount: "$550.00",
-    paymentMethod: "PayPal",
-  },
-  {
-    invoice: "INV006",
-    paymentStatus: "Pending",
-    totalAmount: "$200.00",
-    paymentMethod: "Bank Transfer",
-  },
-  {
-    invoice: "INV007",
-    paymentStatus: "Unpaid",
-    totalAmount: "$300.00",
-    paymentMethod: "Credit Card",
-  },
-];
 
 export function TableDemo({
   DataRow,
@@ -63,24 +18,28 @@ export function TableDemo({
   DataNumbers: number[];
   totalCapacity: number;
 }) {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    if (window) {
+      window.innerWidth <= 640 ? setIsMobile(true) : null;
+    }
+  }, []);
+
   return (
-    <Table>
-      {/* <TableCaption>A list of your recent invoices.</TableCaption> */}
+    <Table className={`${isMobile && "flex"}`}>
       <TableHeader>
-        <TableRow>
+        <TableRow className={`${isMobile && "flex flex-col"}`}>
           {DataRow.map((item, index) => (
-            <TableHead key={index} className="w-[100px]">
+            <TableHead key={index} className="">
               {item}
             </TableHead>
           ))}
-          <TableHead className="w-[100px]">Total</TableHead>
-          {/* <TableHead>Status</TableHead>
-          <TableHead>Method</TableHead>
-          <TableHead className="text-right">Amount</TableHead> */}
+          <TableHead className="">Total</TableHead>
         </TableRow>
       </TableHeader>
       <TableBody>
-        <TableRow>
+        <TableRow className={`${isMobile && "flex flex-col"}`}>
           {DataNumbers.map((item, index) => (
             <TableCell key={index} className="font-medium">
               {item}
@@ -89,19 +48,8 @@ export function TableDemo({
           <TableCell className="font-medium">
             {totalCapacity.toFixed(2)}%
           </TableCell>
-          {/* {invoices.map((invoice) => (
-            <TableCell>{invoice.paymentStatus}</TableCell>
-            <TableCell>{invoice.paymentMethod}</TableCell>
-            <TableCell className="text-right">{invoice.totalAmount}</TableCell>
-          ))} */}
         </TableRow>
       </TableBody>
-      {/* <TableFooter>
-        <TableRow>
-          <TableCell colSpan={3}>Total</TableCell>
-          <TableCell className="text-right">$2,500.00</TableCell>
-        </TableRow>
-      </TableFooter> */}
     </Table>
   );
 }

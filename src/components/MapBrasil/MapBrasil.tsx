ac07b57b20e319a243f20fb29a6ad659b3c244fc
ViniHGV <ixxvinicius@gmail.com>
2024-01-09 21:18:59 -0300

    feat: refatorando mapBrasil

--- src/components/MapBrasil/MapBrasil.tsx
+++ src/components/MapBrasil/MapBrasil.tsx
@@ -3,51 +3,18 @@ import React, { useContext, useEffect, useState } from "react";
 import { SVGMap } from "react-svg-map";
 import Brazil from "@svg-maps/Brazil";
 import "./style.css";
-import { DataBase } from "../../../data/data";
-import { toast } from "react-toastify";
 import { appContext } from "../../../context/appContext";
-import { useApiContext } from "../../../context/apiContext";
-
-type IMapBrasil = {
-  HandleStateProp: (ev: any) => void;
-  sumTotalCapacityState: number;
-  setSumTotalCapacityState: React.Dispatch<React.SetStateAction<number>>;
-  sumDefaut: number;
-  setSumDefaut: React.Dispatch<React.SetStateAction<number>>;
-};
 
 export const MapBrasil = () => {
-  const [tooltipState, setTooltipState] = useState({
-    show: false,
-    x: 0,
-    y: 0,
-    text: "",
-  });
   const {
-    setSumDefaut,
-    setSumLC,
     setStateSelectedToDoMap,
-    setREPlusLC,
-    setFullRE,
-    setCCGT,
-    setHygrogen,
-    setNuclear,
-    setOnshorewind,
-    setPVexisting,
-    setReservoir,
-    setRunofriver,
-    setUtilityscalePV,
-    setBaseline,
-    setIntensiveElec,
-    setLimitedElec,
-    setNetZero,
     selectedCheckboxesState,
     setSelectedCheckboxesState,
     dataNumbers,
     dataDescription,
+    toolbarStateMap,
+    setToolbarStateMap,
   }: any = useContext(appContext);
-
-  const { setUrlFetchApiFilter, dataFilteredAPI } = useApiContext();
 
   const handleChange = (
     event?: React.ChangeEvent<HTMLInputElement>,
@@ -71,36 +38,17 @@ export const MapBrasil = () => {
   const handleStatesMap = (ev: Event | any) => {
     let state = ev.target.id.toUpperCase();
 
-    setTooltipState({ show: false, x: 0, y: 0, text: "" });
+    setToolbarStateMap({ show: false, x: 0, y: 0, text: "" });
     handleLocationMouseOver(ev);
 
     setStateSelectedToDoMap(state);
-    setUrlFetchApiFilter((prev) => prev.concat(`&state=${state}`));
-    setFullRE(dataFilteredAPI.totals?.policy["100% RE"]);
-    setREPlusLC(dataFilteredAPI.totals?.policy["100% RE + LC"]);
-    setSumLC(dataFilteredAPI.totals?.policy["+LC"]);
-    setSumDefaut(dataFilteredAPI.totals?.policy["Default"]);
-
-    setCCGT(dataFilteredAPI.totals?.techs["CCGT"]);
-    setHygrogen(dataFilteredAPI.totals?.techs["Hydrogen"]);
-    setNuclear(dataFilteredAPI.totals?.techs["Nuclear"]);
-    setOnshorewind(dataFilteredAPI.totals?.techs["Onshore wind"]);
-    setPVexisting(dataFilteredAPI.totals?.techs["PV-existing"]);
-    setReservoir(dataFilteredAPI.totals?.techs["Reservoir"]);
-    setRunofriver(dataFilteredAPI.totals?.techs["Run-of-river"]);
-    setUtilityscalePV(dataFilteredAPI.totals?.techs["Utility-scale PV"]);
-
-    setBaseline(dataFilteredAPI.totals?.scenario["Baseline"]);
-    setIntensiveElec(dataFilteredAPI.totals?.scenario["Intensive elec"]);
-    setLimitedElec(dataFilteredAPI.totals?.scenario["Limited elec"]);
-    setNetZero(dataFilteredAPI.totals?.scenario["Net zero"]);
   };
 
   const handleLocationMouseOver = (e: any) => {
     const stateName = e.target.id;
 
     if (stateName) {
-      setTooltipState({
+      setToolbarStateMap({
         show: true,
         x: e.pageX,
         y: e.pageY,
@@ -125,12 +73,12 @@ export const MapBrasil = () => {
           handleChange(ev, ev.target.id.toUpperCase());
         }}
       />
-      {tooltipState.show && (
+      {toolbarStateMap.show && (
         <div
           className="absolute bg-black text-white p-2 rounded-lg shadow-lg"
           style={{
-            left: tooltipState.x - 100,
-            top: tooltipState.y - 325,
+            left: toolbarStateMap.x - 100,
+            top: toolbarStateMap.y - 325,
             transform: "translate(-50%, -100%)",
           }}
         >
